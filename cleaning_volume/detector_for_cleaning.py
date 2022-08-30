d05e0e89e17bde6f5d64647ed1568daaac6f7f9d
Razwand <asanzrod@hotmail.com>
2022-08-30 12:30:56 +0200

    Comment translation

--- cleaning_volume/detector_for_cleaning.py
+++ cleaning_volume/detector_for_cleaning.py
@@ -31,23 +31,15 @@ pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesserac
 
 def build_path(sample, type_path):
     '''
-    Genera una string con el path que se pase como parámetro a la función.
+    This function generates a  string with the path given.
     INPUT
-    - sample: string que contiene el nombre del directorio que contiene el tomo que se está tratando
-    - type_path: puede ser input o output, según el directorio/archivo al que queramos apuntar
+    - sample: string that contains the volume directory name 
+    - type_path: input/output, depending on the directory/file we are pointing at
     '''
-
     if type_path == 'input':
         return('./input_pages/pdf_pages_' + sample + '/')
     elif type_path == 'output':
-        return('./output/Resultado_General_'+sample+'.csv')
-
-def create_folder(name):
-    '''
-    Creates folder named <name> 
-    '''
-    if os.path.isdir(name) == False:
-        os.mkdir(name)
+        return('./output/Final_Result_'+sample+'.csv')
 
 def build_df_total(path):
 
@@ -167,21 +159,19 @@ def check_key_words(text, type_search):
 def detect_cabeceras(img):
 
     '''
-    Función que toma una imagen (img) y devuelve el valor 1 si se ha detectado un fax y 0
-    en caso contrario.
+    This function takes an image (img) yand returns 1 if a fax has been detected. Otherwise, it returns 0.
 
-    Se realizan transformaciones a la imagen de entrada y se extrae el texto que pueda aparecer en ella para
-    buscar las palabras clave que identifiquen al fax
+    Some transformations of the input image are performed and text is extracted in order to search key words that
+    may help identifying a fax page.
     
     '''
-
-    # Tratamiento imagen
+    # Image Treatment
     new_h_fax = int(np.round(img.shape[0]*0.07))  
     im_reduced_fax= img[0:new_h_fax, 0:]
     new_w_fax = int(np.round(im_reduced_fax.shape[1]*0.7))
     im_reduced_fax= im_reduced_fax[0:, 0:new_w_fax]
 
-    # Tratamiento texto de imagen
+    # Text Treatment
     text_img_fax = take_text(im_reduced_fax)
     has_fax = check_key_words(text_img_fax, 'fax')
 
@@ -190,51 +180,50 @@ def detect_cabeceras(img):
 def extract_shield(img,lim_inf_siz_pag, lim_sup_siz_pag, per_new_h,per_new_w,per_new_h_text,per_new_w_text, area_limit_sup):
 
     '''
-    Función que a partir de una imagen devuelve 1 si se ha detectado
-    un escudo con texto asociado ADMINISTRACION DE JUSTICIA y 0 en caso contrario.
+    Function that returns 1 if a shield (from ADMINISTRACION DE JUSTICIA) is detected and 0 otherwise.
     
     '''
-    # Flag de detección de escudo administración
+    # Flag for shield detection
     admin  = 0
 
-    # Filtrado de páginas que no son A4
-    # imshape[1] = ancho, imshape[0] = alto
+    # Filtering images that are not A4
+    # imshape[1] = width, imshape[0] = height
     keepPage = img.shape[1]>lim_inf_siz_pag and img.shape[1]<lim_sup_siz_pag 
     
-    # Reducir imagen a esquina superior izquierda
+    # Reducing image to left sup corner
     new_h = int(np.round(img.shape[0]*per_new_h)) 
     im_reduced= img[0:new_h, 0:]
     new_w = int(np.round(im_reduced.shape[1]*per_new_w))  
     im = im_reduced[0:,0:new_w]
     
-    # Reducción específica para extraer texto
+    # pecific reduction to extract text
     new_h_text = int(np.round(img.shape[0]*per_new_h_text))  
     im_reduced_text= img[0:new_h_text, 0:]
     new_w_text = int(np.round(im_reduced_text.shape[1]*per_new_w_text)) 
     im_text= im_reduced_text[0:, 0:new_w_text]
      
-    # Transformación de BGR a RGB, escala de gris y binario (simplificar pixeles sobre 150 a 255)
+    # BGR to RGB transformation, greyscale  and binary (simplifying pixels over 150 to 255)
     rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY) 
     binary = cv2.threshold(gray, 150, 255,cv2.THRESH_BINARY_INV)[1]  
 
-    # Componentes
+    # Components
     connectivity = 4 
     numLabels, labels, stats, centroids = cv2.connectedComponentsWithStats(binary,connectivity , cv2.CV_32S)
 
-    # Ancho y Alto de Imagen Origen (equina sup izqda)
+    # Width, height of the original image (left sup corner)
     max_alto = binary.shape[0]
 
     if keepPage == True:
-        # Para cada una de las componentes detectadas... 
+        # For each one of the components... 
         for i in range(1, numLabels):
 
-            # Ancho, Alto y Area de la componente
+            # Height, Width and Area
             w = stats[i, cv2.CC_STAT_WIDTH]
             h = stats[i, cv2.CC_STAT_HEIGHT]
             area = stats[i, cv2.CC_STAT_AREA]
 
-            # Filtros componentes
+            # Filtering Components
             keepWidth = w > 80  
             keepHeight = h > 90 and h < max_alto 
             keepArea = area < area_limit_sup  
@@ -242,7 +231,7 @@ def extract_shield(img,lim_inf_siz_pag, lim_sup_siz_pag, per_new_h,per_new_w,per
             keepNotBanners = h>=w 
 
             if keepWidth and keepHeight and keepNotBanners and keepArea and keepSquared:
-                # Extracción de texto si la componente pasa el filtro
+                # Text extraction if image pass the filter
                 text_img = take_text(im_text)
                 admin = check_key_words(text_img,'spain')
 
@@ -251,8 +240,7 @@ def extract_shield(img,lim_inf_siz_pag, lim_sup_siz_pag, per_new_h,per_new_w,per
 def detect_shield(img):
 
     '''
-    Función que inicializa los parámetros que se utilizarán en 
-    la función de detección de escudos de administración
+    Function that initialize parameters that will be used when detecting the shield
     '''
 
     lim_inf_siz_pag =1500
@@ -263,18 +251,17 @@ def detect_shield(img):
     per_new_w_text = 0.3
     area_limit_sup = 40000
 
-    
-    
     return(extract_shield(img,lim_inf_siz_pag, lim_sup_siz_pag, per_new_h,per_new_w,per_new_h_text,per_new_w_text, area_limit_sup))
 
-# Separación en PDFS
+# PDF splitting
 def build_df(list_pages,sample,name_doc):
 
     '''
-    Esta función selecciona las páginas filtradas y devuelve
-    una versión del tomo con dichas páginas en la carpeta output.
+    This function selects the filtered pages and return a volume version with only this pages and 
+    save it in the output folder.
 
-    El nombre de éste será <sample>_filtered.pdf
+    The name of the file will be:
+     <sample>_filtered.pdf
     '''
     
     path = './input_pages/pdf_pages_'+sample +'/'
@@ -288,12 +275,12 @@ def build_df(list_pages,sample,name_doc):
 def extract_page_info(path_img, obj):
 
     '''
-    Esta función lee la imagen (path_img) que corresponde a una página a partir de su path y según
-    el objeto (obj) de búsqueda llama a funciones específicas de extracción.
-    obj
-    - <emp_page>: búsqueda de hojas vacías
-    - <fax_page>: búsqueda de fax
-    - <shield>: búsqueda de escudos de administración
+    This function reads an image (path_img) that corresponds to a page and given the search object (obj) 
+    a specific extraction function will be called.
+    obj:
+    - <emp_page>: empty pages search
+    - <fax_page>: fax search
+    - <shield>: shield search
     
     '''
     im_orig = cv2.imread(path_img)
@@ -360,12 +347,11 @@ def check_args(argv):
     '''
     Function checking input arguments.
     '''
-    print(argv)
     if len(argv) == 3 and argv[2] in ['PDF', 'IMG']:
         if argv[2] == 'PDF':
             create_folder('input_pages')
             if os.path.isdir('input_volume') and len(os.listdir('./input_volume/') ) != 0 and len(os.listdir('./input_volume/'+ argv[1]) ) != 0:
-                print('[\U0001F642 ] Correct Input Path: {}'.format('./input_volume/' + argv[1] + '/'))
+                print('\U0001F642 Correct Input Path: {}'.format('./input_volume/' + argv[1] + '/'))
                 return(True)
             else:
                 print('Please, include the pdf folder inside the directory /input_volume')
@@ -379,7 +365,7 @@ def check_args(argv):
                 return(False)
 
     else:
-         print('[ERROR]: Incorrect number of arguments. Arguments should be: name of the directory to be treated and execution mode (PDF or IMG)')     
+         print('\U0001F4A5 Incorrect number of arguments. Arguments should be: name of the directory to be treated and execution mode (PDF or IMG)')     
 
 if __name__ == "__main__":
 
--- cleaning_volume/split_pdf.py
+++ cleaning_volume/split_pdf.py
@@ -10,7 +10,7 @@ poppler_path = 'C:/Users/asr_l/miniconda3/envs/dealing_with_docs/Lib/site-packag
 def create_folder(name):
 
     '''
-    Crea una carpeta con el nombre <name> si no existe ya
+    Creates a folder with name <name> if it doesn't exist
     '''
     
     if os.path.isdir(name) == False:
@@ -19,26 +19,25 @@ def create_folder(name):
 def pdf_to_image(input_folder_name,poppler_path, output_folder_path):
 
     '''
-    Esta función toma un pdf y dado el nombre de la carpeta de input
-    genera imágenes para cada una de las páginas en una carpeta
+    This fuction takes a pdf and, given de input folder name,
+    generate images for each one of the pdf pages.
     
     pdf_pages_<input_folder_name>
     
-    El nombre de las imágenes creada seguirá la estructura:
+    The name of the images created will have the following structure:
 
-    <nombre_del_tomo>.pdf_page_<numero_de_pagina>.jpg
+    <name_of_the_volume>.pdf_page_<page_number>.jpg
     '''
 
-    tomo = os.listdir('./input_volume/'+input_folder_name+'/')[0]
-    print(tomo)
+    volume = os.listdir('./input_volume/'+input_folder_name+'/')[0]
     out_path_pdf = output_folder_path +'pdf_pages_'+input_folder_name+'/'
     create_folder(out_path_pdf)
 
-    pages = convert_from_path('./input_volume/' + input_folder_name +'/'+tomo, 200,poppler_path=poppler_path)
+    pages = convert_from_path('./input_volume/' + input_folder_name +'/'+volume, 200,poppler_path=poppler_path)
     
     for i in range(len(pages)):
-        path_page_img = output_folder_path +'pdf_pages_'+ input_folder_name +'/' + tomo + '_page_'+str(i)+'.jpg'
-        print('Saving page image: ', path_page_img)
+        path_page_img = output_folder_path +'pdf_pages_'+ input_folder_name +'/' + volume + '_page_'+str(i)+'.jpg'
+        print('⏳ Saving page image: ', path_page_img)
         pages[i].save(path_page_img, 'JPEG')     
 
 
